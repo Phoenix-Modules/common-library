99ccf33cc7a0c937b02aee141886f7c7e88e6376
Brandin Canterbury <brandincanterbury@gmail.com>
2024-06-18 12:09:53 -0400

    start compendium service work

---
+++ src/constants/compendiumTypes.js
@@ -0,0 +1,4 @@
+export const compendiumTypes = {
+    Item: "Item",
+    Macro: "Macro"
+}
---
+++ src/services/compendiumService.js
@@ -0,0 +1,27 @@
+
+
+
+export async function addToCompendium(featureArray) {
+    const packName = `${moduleData.moduleName}.${moduleData.featurePack}`;
+    const pack = game.packs.find(p => p.metadata.id === packName);
+    if (!pack) {
+        throw new Error("Compendium {flight-features} not found");
+    }
+
+    await addManyToPack(pack, featureArray, "item");
+}
+
+async function addManyToPack(pack, itemCollection) {
+    pack.configure({ locked: false });
+
+    for (const item of itemCollection) {
+        const existingItem = pack.index.find((i) => i.name === item.name);
+        if (!existingItem) {
+            let addedItem;
+            addedItem = await Item.create(item, { pack: pack.metadata.id });
+            console.log(`Macro '${addedItem.name}' added to compendium ${pack.metadata.name}`);
+        }
+    }
+
+    pack.configure({ locked: true });
+}
