f589f3bf6b29ae4771520af0d85fa4e1545f2ad0
Brandin Canterbury <BCanterbury@firstkeyhomes.com>
2024-06-26 17:26:38 -0400

    Another attempt at rollup/vite

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import * as Services from './services/index.js';
 import * as Constants from './constants/index.js';
 
-
-module.exports = {
-    Services,
-    Constants
-}
+exports.Services = Services;
+exports.Constants = Constants;
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+﻿import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        minify: false,
+        rollupOptions: {
+            plugins: [nodeResolve({ exportConditions: ['node'] })],
+            input: {
+                module: "src/index.js",
+            },
+            output: {
+                entryFileNames: "index.js",
+                format: "cjs",
+                dir: "dist",
+                interop: "esModule"
+            }
+        },
+    }
+});
